ce126666ec81f8349aacaa17f24299bbd5ee52c8
Aditya Kumar <adityakrchy101@gmail.com>
2025-07-12 01:50:32 +0530

    minor changes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,37 @@
-import app from '../src/app';
-import { createServer } from 'http';
+require('dotenv').config();
 
-export default function handler(req: any, res: any) {
-  const server = createServer(app);
-  server.emit('request', req, res);
-}
+const express = require('express');
+const app = express();
+const { sql } = require('@vercel/postgres');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.json({ message: 'Hello from Express + Vercel!' });
+});
+
+app.get('/api/hello', function (req, res) {
+  res.json({ message: 'Hello from Express + Vercel!' });
+});
+
+app.get('/api/hello2', function (req, res) {
+  res.json({ message: 'Hello from Express + Vercel!' });
+});
+
+app.get('/api/hello3', function (req, res) {
+  res.json({ message: 'Hello from Express + Vercel!' });
+});
+
+
+
+
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
